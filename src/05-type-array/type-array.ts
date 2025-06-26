a96b5dab3a0f0a50081026ff87e0edf6b6aa55ca
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-26 10:11:59 -0300

    Readonly arrays

--- src/05-type-array/type-array.ts
+++ src/05-type-array/type-array.ts
@@ -3,3 +3,6 @@ function multArgs(...args: Array<number>): number {
 }
 
 console.log(multArgs(1, 2, 3));
+
+const numbers: readonly number[] = [1, 2, 3, 4, 5, 6];
+const numbers2: readonly ReadonlyArray<number> = [1, 2, 3, 4, 5, 6];
